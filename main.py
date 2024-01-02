328d72be3c9a13f62c0d65f104f998d0f2732ab5
Arman Aydemir <arman.aydemir@live.com>
2024-01-02 11:09:56 -0800

    pydantic bot request class

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 # Create hello world FastAPI app
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class BotRequest(BaseModel):
+    history: list
+    t1name: str | None = None
+    t1txt: str | None = None
+    t1prompt: str | None = None
+    t2name: str | None = None
+    t2txt: str | None = None
+    t2prompt: str | None = None
+    user_prompt: str | None = None
+    session: str | None = None
+    bot_id: str | None = None
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
     return {"message": "It's OpenProBono !!"}
+
+@app.post("/bot")
+def bot(request: BotRequest):
+    return request
