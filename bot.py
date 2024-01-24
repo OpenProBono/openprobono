82604b87f8577fd55a1dc88172272b6149a9b813
Arman Aydemir <arman.aydemir@live.com>
2024-01-23 21:54:53 -0800

    stupid copy all imports

--- bot.py
+++ bot.py
@@ -1,3 +1,41 @@
+import uuid
+
+import firebase_admin
+import langchain
+from typing import Annotated, List, Union, Tuple
+from fastapi import Body, FastAPI
+from firebase_admin import credentials, firestore
+import gradio as gr
+from langchain import PromptTemplate
+from langchain.agents import (AgentExecutor, AgentOutputParser, AgentType,
+                              LLMSingleActionAgent, Tool, ZeroShotAgent,
+                              initialize_agent)
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.callbacks.streaming_stdout_final_only import \
+    FinalStreamingStdOutCallbackHandler
+from langchain.chains import LLMChain, RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import (TextLoader, UnstructuredURLLoader,
+                                        YoutubeLoader)
+from langchain.document_loaders.blob_loaders.youtube_audio import \
+    YoutubeAudioLoader
+from langchain.document_loaders.generic import GenericLoader
+from langchain.document_loaders.parsers import OpenAIWhisperParser
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.memory import ChatMessageHistory, ConversationBufferMemory
+from langchain.prompts import (BaseChatPromptTemplate, MessagesPlaceholder,
+                               PromptTemplate)
+from langchain.schema import AgentAction, AgentFinish, AIMessage, HumanMessage
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from pydantic import BaseModel
+
+from anyio.from_thread import start_blocking_portal
+from queue import Queue
+import re
+from serpapi.google_search import GoogleSearch
+
 # OPB bot main function
 def opb_bot(
     history,
