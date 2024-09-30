2c75b7f2339d171f124db713ab9deac76c3d42b7
Nick Colvin <njc@alumni.unc.edu>
2024-09-30 14:41:06 -0400

    git testing

--- app/milvusdb.py
+++ app/milvusdb.py
@@ -54,6 +54,9 @@ vdb_log.setLevel(logging.ERROR)
 vdb_log.addHandler(my_handler)
 
 
+# test env vars
+print(os.environ)
+
 # used to cache collection params from firebase
 COLLECTION_PARAMS = {}
 # for storing session data
